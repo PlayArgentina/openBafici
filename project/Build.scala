c7af428b07f304477f937ae94520eb9e8e654f9a
Nicolás Melendez <nfmelendez@gmail.com>
2012-05-05 13:52:26 -0300

    Config restOpenGov4j

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+    "ar.com.restba" % "restba" % "1.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+    resolvers += ("Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository") 
     )
 
 }
