e0aff18c3c55fb989d6cd4ce3d320bf6f7e66fcf
opensas <opensas@gmail.com>
2012-05-05 17:26:10 -0300

    calling web service

--- app/controllers/Films.scala
+++ app/controllers/Films.scala
@@ -4,54 +4,43 @@ import play.api._
 import play.api.mvc._
 
 import play.api.libs.ws.WS
+import play.api.libs.json.Json
 
 import models.Film
-
-import utils.Bafici
 
 object Films extends Controller {
   
   def list = Action {
 
     val films = Seq(
-      Film(1, "La banda del openBafici"),
-      Film(2, "La banda del openBafici II")
+      Film("1", "La banda del openBafici"),
+      Film("2", "La banda del openBafici II")
     )
 
     Ok(views.html.list(films = films))
   }
 
   def show(id: String) = Action {
-    val film = Film(1, "La banda del openBafici")
-    Ok(views.html.show(film))
+    //2732fbf4-4e55-4794-8e98-e5d5fa6a0419-4
+    val film = Film.findById(id)
+    film.map { film =>
+      Ok(views.html.show(film))  
+    }.getOrElse {
+      NotFound
+    }
+
   }
 
   def test() = Action {
-    var query = Bafici.endpoint + """_search?q=synopsis_es:'buenos aires'"""
-
-    query = "http://zenithsistemas.com:9200/"
-
-    query = "http://zenithsistemas.com:9200/gcba/bafici/2732fbf4-4e55-4794-8e98-e5d5fa6a0419-4"
+    var query = Film.endpoint + """_search?q=synopsis_es:'buenos aires'"""
 
     query = "http://zenithsistemas.com:9200/gcba/bafici/2732fbf4-4e55-4794-8e98-e5d5fa6a0419-4?fields=id,title,title_es,url_ticket,year,generes_list,cast,id_youtube,filepic1,prodteam,synopsis_es,synopsis_en,duration,director,updated_ts"
 
-    //Ok(views.html.test(query,"body"))
-
     Async {
-      val ws = WS.url(query)
-      val wsGet = ws.get()
-
       WS.url(query).get().map { response =>
-
-        val body = response.body
-
-        //val json = response.json
-
-        //val title = (json).as[String]
-
-        Ok(
-          views.html.test(query, body)
-        )
+        val json = Json.parse(response.body)
+        val pic = (json \ "fields" \ "filepic1").as[String]
+        Ok(views.html.test(query, pic))
       }
     }
 
--- app/models/Film.scala
+++ app/models/Film.scala
@@ -8,26 +8,78 @@ import play.api.Play.current
 import anorm._
 import anorm.SqlParser._
 
+import play.Play.application
+import play.api.libs.ws.WS
+import play.api.libs.json.{Json, JsValue}
+
 case class Film(
-  id: Long = 0, 
-  title: String = "unknown film",
-  title_es: String = "desconocido",
-  url_ticket: String = "http://google.com",
-  year: Long = 1984,
-  generes_list: String = "ninguno genero",
-  cast: String = "Van Damme",
-  id_youtube: String = "werwerwer",
-  filepic1: String = "image.png",
-  prodteam: String = "Scorcese",
-  synopsis_es: String = "sinopsis",
-  synopsis_en: String = "sinopsis",
-  duration: Long = 95,
-  director: String = "Fulano",
-  updated_ts: String = "20-20-1945"
+  id: String            = "",
+  title: String         = "unknown film",
+  title_es: String      = "desconocido",
+  url_ticket: String    = "http://google.com",
+  year: Long            = 1984,
+  generes_list: String  = "ninguno genero",
+  cast: String          = "Van Damme",
+  id_youtube: String    = "werwerwer",
+  filepic1: String      = "image.png",
+  prodteam: String      = "Scorcese",
+  synopsis_es: String   = "sinopsis",
+  synopsis_en: String   = "sinopsis",
+  duration: Long        = 95,
+  director: String      = "Fulano",
+  updated_ts: String    = "20-20-1945"
 )
 
 object Film {
   
+ val fields: String = 
+    "id,title,title_es,url_ticket,year,generes_list,cast,id_youtube,filepic1," +
+    "prodteam,synopsis_es,synopsis_en,duration,director,updated_ts"
+
+  val endpoint = application.configuration.getString("bafici.endpoint") match { 
+    case null => "" case s: String => s 
+  }
+
+  def apply(json: JsValue): Film = {
+
+    val fields = json \ "fields"
+    
+    Film(
+      id                  = readJson(fields, "id", ""),
+      title               = readJson(fields, "title", "unknown film"),
+      title_es            = readJson(fields, "title_es", "desconocido"),
+      url_ticket          = readJson(fields, "url_ticket", "http://google.com"),
+      year                = readJsonLong(fields, "year", 1984),
+      generes_list        = readJson(fields, "generes_list", "ninguno genero"),
+      cast                = readJson(fields, "cast", ""),
+      id_youtube          = readJson(fields, "id_youtube", ""),
+      filepic1            = readJson(fields, "filepic1", ""),
+      prodteam            = readJson(fields, "prodteam", ""),
+      synopsis_es         = readJson(fields, "synopsis_es", ""),
+      synopsis_en         = readJson(fields, "synopsis_en", ""),
+      duration            = readJsonLong(fields, "duration", 90),
+      director            = readJson(fields, "director", ""),
+      updated_ts          = readJson(fields, "updated_ts", "")
+    )
+
+  }
+
+  private def readJson(node: JsValue, field: String, default: String): String = {
+    (node \ field).asOpt[String].map { value =>
+      value
+    }.getOrElse {
+      default
+    }
+  }
+
+  private def readJsonLong(node: JsValue, field: String, default: Long): Long = {
+    (node \ field).asOpt[Long].map { value =>
+      value
+    }.getOrElse {
+      default
+    }
+  }
+
   // -- Parsers
   
   /**
@@ -36,18 +88,21 @@ object Film {
 
   val simple = { 
 
-    get[Long]("film.id") ~
+    get[String]("film.id") ~
     get[String]("film.name") map {
       case id~name => Film(id, name)
     }
   }
 
-  val fields: String = 
-    "id,title,title_es,url_ticket,year,generes_list,cast,id_youtube,filepic1," +
-    "prodteam,synopsis_es,synopsis_en,duration,director,updated_ts"
+  def findById(id: String): Option[Film] = {
+    val query = endpoint + id + "?fields=" + fields
 
+    val response = WS.url(query).get().await.get
 
+    val json = Json.parse(response.body)
+    Option[Film](Film(json))
 
+  }
 
 }
 
