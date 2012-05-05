05ef2ed07c1cfc81d965e7aa03795d895592971c
Walter Javier Franck <walterfranck@yahoo.com.ar>
2012-05-05 12:39:28 -0300

    Model filled, inital action to List

--- app/models/Film.scala
+++ app/models/Film.scala
@@ -10,7 +10,20 @@ import anorm.SqlParser._
 
 case class Film(
   id: Long = 0, 
-  title: String = "unknown film"
+  title: String = "unknown film",
+  title_es: String = "desconocido",
+  url_ticket: String = "http://google.com",
+  year: Long = 1984,
+  generes_list: String = "ninguno genero",
+  cast: String = "Van Damme",
+  id_youtube: String = "werwerwer",
+  filepic1: String = "image.png",
+  prodteam: String = "Scorcese",
+  synopsis_es: String = "sinopsis",
+  synopsis_en: String = "sinopsis",
+  duration: Long = 95,
+  director: String = "Fulano",
+  updated_ts: String = "20-20-1945"
 )
 
 object Film {
