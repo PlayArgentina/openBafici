5caa0334b1dde98ddf434a2ebebdd01b4a1781da
Walter Javier Franck <walterfranck@yahoo.com.ar>
2012-05-05 18:19:02 -0300

    mejoras en fe

--- app/controllers/Films.scala
+++ app/controllers/Films.scala
@@ -27,7 +27,7 @@ object Films extends Controller {
       println("JSON: " + item)
       try {
       val film = Film(
-	      0, 
+	      "0", 
 		  item.getString("title"),
 		  item.getString("title_es"),
 		  item.getString("url_ticket"),
